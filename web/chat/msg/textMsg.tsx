5eb63d9191e4126df15f36fab1b83303193c7f00
Jesse Bradford <JPB@macbook-air.lan>
2020-08-30 11:23:35 -0500

    clickable links in messages

--- web/chat/msg/textMsg.tsx
+++ web/chat/msg/textMsg.tsx
@@ -4,28 +4,41 @@ import {openLink} from '../../utils/openLink'
 import { ReactTinyLink } from 'react-tiny-link-electron'
 import {useParsedGiphyMsg} from '../../../src/store/hooks/msg'
 import { message } from '../../../src/store/websocketHandlers'
+import Linkify from 'react-linkify';
 
 export default function TextMsg(props){
   const {message_content} = props
-  const isLink = message_content && (message_content.toLowerCase().startsWith('http://') || message_content.toLowerCase().startsWith('https://'))
+  var messageArray = message_content.split(` `)
+  var urlRegex = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi
+  var link = null
+
+  messageArray.forEach(function (element, index) {
+    if(urlRegex.test(element)){
+      if(!element.startsWith(`https://`)){element = `https://` + element}
+      link = element
+      // messageArray[index] = '{<a href="' + element + '">' + element + '</a>}';
+    }
+  });
+  console.log(link)
+  const hasLink = message_content && link
   
-  if (isLink) {
-    return <LinkWrap>
-      <Link href={message_content} target="_blank" onClick={(e:any)=>{
+  if (hasLink) {
+    return <Wrap>
+      <Linkify component='button' properties={{onClick: function onClick() { alert( 'Success!'); }}}>{messageArray.join(` `)}</Linkify>
+      <Link href={link} target="_blank" onClick={(e:any)=>{
         e.preventDefault()
         const target:any = event.target
         openLink(target.href)
       }}>
-        {message_content}
       </Link>
       <ReactTinyLink
         cardSize="small"
         showGraphic={true}
         maxLine={2}
         minLine={1}
-        url={message_content}
+        url={link}
       />
-    </LinkWrap>
+    </Wrap>
   }
 
   const isGiphy = message_content && message_content.startsWith('giphy::')
