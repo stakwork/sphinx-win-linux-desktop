6ed8fd793ec27e398a0c1a85426f8b0ad1dd9cde
Evan Feenstra <evan@evanfeenstra.com>
2020-11-03 10:21:47 -0800

    fix windows/linux signup w code

--- src/store/chats.ts
+++ src/store/chats.ts
@@ -82,6 +82,7 @@ export class ChatStore {
 
   @action
   gotChat(chat: Chat) {
+    console.log("====> GOT CHAT", chat)
     const existingIndex = this.chats.findIndex(ch=>ch.id===chat.id)
     if(existingIndex>-1){
       this.chats[existingIndex] = chat
