1c6af5015cd26096447574711444e80ee5ed8cfe
Jesse Bradford <JPB@macbook-air.lan>
2021-02-11 13:19:54 -0600

    paid Images testing success

--- test-ava/functions/send-image.js
+++ test-ava/functions/send-image.js
@@ -5,9 +5,10 @@ var fetch = require('node-fetch')
 var RNCryptor = require('jscryptor-2')
 var getContacts = require('./get-contacts')
 var getTribeId = require('./get-tribe-id')
+var getChats = require('./get-chats')
 var h = require('../helpers/helper-functions')
 
-async function sendImage(t, node1, node2, image, tribe){
+async function sendImage(t, node1, node2, image, tribe, price){
 //NODE1 SENDS AN IMAGE TO NODE2
 
     var token = await h.getToken(t, node1)
@@ -24,88 +25,115 @@ async function sendImage(t, node1, node2, image, tribe){
 
     const [n1contactP1, n2contactP1] = await getContacts(t, node1, node2)
 
-    let encrypted_media_key
+    //encrypt media_key with node1 contact_key, node1 perspective
+    let encrypted_media_key = rsa.encrypt(n1contactP1.contact_key, upload.media_key)
     let encrypted_media_key2
+    let contactIdP1 = null
+    let tribeIdP1 = null
+    let mediaKeyMap = null
     if(tribe){
-      //encrypt media_key with node2 contact_key, node1 perspective
-      encrypted_media_key = rsa.encrypt(n1contactP1.contact_key, upload.media_key)
-      //encrypt media_key with node1 contact_key, node2 perspective
+      //encrypt media_key with tribe group_key
       encrypted_media_key2 = rsa.encrypt(tribe.group_key, upload.media_key)
-    }
-    else{
-      //encrypt media_key with node2 contact_key, node1 perspective
-      encrypted_media_key = rsa.encrypt(n1contactP1.contact_key, upload.media_key)
-      //encrypt media_key with node1 contact_key, node2 perspective
-      encrypted_media_key2 = rsa.encrypt(n2contactP1.contact_key, upload.media_key)
-    }
+      tribeIdP1 = await getTribeId(t, node1, tribe)
+      mediaKeyMap = {["chat"]: encrypted_media_key2, [n1contactP1.id]: encrypted_media_key}
 
+    } else{
+      //encrypt media_key with node2 contact_key, node1 perspective
+      encrypted_media_key2 = rsa.encrypt(n2contactP1.contact_key, upload.media_key)
+      contactIdP1 = n2contactP1.id
+      mediaKeyMap = {[n2contactP1.id]: encrypted_media_key2, [n1contactP1.id]: encrypted_media_key}
+    }
 
     //media key map is  
     //person_sending_to: person_sending_to_contact_key,
     //person_sending: person_sending_contact_key
 
-    //create empty media of object
-    let i = {}
-
-    //If tribe, create chat media object
-    if(tribe){
-      const tribeId = await getTribeId(t, node1, tribe)
-
-      i = {
-        contact_id: null,
-        chat_id: tribeId,
+    //create 
+    let i = {
+        contact_id: contactIdP1,
+        chat_id: tribeIdP1,
         muid: upload.muid,
-        media_key_map: {["chat"]: encrypted_media_key2, [n1contactP1.id]: encrypted_media_key},
+        media_key_map: mediaKeyMap,
         media_type: 'image/jpg',
         text: '',
         amount: 0,
-        price: 0
-    }
-    }
-    //if no tribe, create contact media object
-    else {
-          i = {
-        contact_id: n2contactP1.id,
-        chat_id: null,
-        muid: upload.muid,
-        media_key_map: {[n2contactP1.id]: encrypted_media_key2, [n1contactP1.id]: encrypted_media_key},
-        media_type: 'image/jpg',
-        text: '',
-        amount: 0,
-        price: 0
-    }
+        price: 0 || price
     }
 
     //send message from node1 to node2
     const img = await http.post(node1.ip+'/attachment', h.makeArgs(node1, i))
     //make sure msg exists
-    t.truthy(img, "sent image should exist")
+    t.true(img.success, "sent image should exist")
 
+    if(price){
+      //IF IMAGE HAS A PRICE ===>
 
-    //get messages from node2 perspective
-    const res = await http.get(node2.ip+'/messages', h.makeArgs(node2));
-    //find last message
-    t.truthy(res.response.new_messages, 'node1 should have at least one message')
-    //extract the last message sent to node2
-    const lastMessage2 = res.response.new_messages[res.response.new_messages.length-1]
-    //get media key from message
-    const node2MediaKey = lastMessage2.media_key
+      //get messages from node2 perspective
+      const prePurchMsgs = await http.get(node2.ip+'/messages', h.makeArgs(node2));
+      //find last message
+      t.truthy(prePurchMsgs.response.new_messages, 'node2 should have at least one message')
+      //extract the last message sent to node2
+      const lastPrePurchMsg = prePurchMsgs.response.new_messages[prePurchMsgs.response.new_messages.length-1]
 
-    const decryptMediaKey = rsa.decrypt(node2.privkey, node2MediaKey)
+      //create contact_id for purchase message
+      const [n2contactP2, n1contactP2] = await getContacts(t, node2, node1)
+      let purchContact = n1contactP2.id
+      //create chat_id for purchase message (in tribe and outside tribe)
+      let purchChat = null
+      if(tribe){
+        purchChat = await getTribeId(t, node2, tribe)
+      } else {
+        const chats = await getChats(t, node2)
+        const sharedChat = chats.find(chat => h.arraysEqual(chat.contact_ids, [1, parseInt(n1contactP2.id)]))
+        t.truthy(sharedChat, "there should be a chat with node1 and node2")
+        purchChat = sharedChat.id
+      }
+      //create media_token for purchase message
+      const mediaToken = lastPrePurchMsg.media_token
 
-    //get media token
-    var token = await h.getToken(t, node2)
+      //create purchase message object
+      let p = {
+          contact_id: purchContact,
+          chat_id: purchChat,
+          amount: price,
+          media_token: mediaToken
+      }
 
-    const url = `https://memes.sphinx.chat/file/${lastMessage2.media_token}`
+      //send purchase message from node2 purchasing node1 image
+      const purchased = await http.post(node2.ip+'/purchase', h.makeArgs(node2, p))
+      t.true(purchased.success, "purchase message should be posted")
+      //await post
+      await h.sleep(1000)
 
-    const res2 = await fetch(url, {headers: {Authorization: `Bearer ${token}`}})
-    const blob = await res2.buffer()
+    } 
+    
+    //CONFIRM AND DECRYPT IMAGE
 
-    //media_key needs to be decrypted with your private key
-    const dec = RNCryptor.Decrypt(blob.toString("base64"), decryptMediaKey)
-    // const b64 = dec.toString('base64')
-    // //check equality b64 to b64
-    t.true(dec.toString("base64") === image)
+        //get messages from node2 perspective
+        const res = await http.get(node2.ip+'/messages', h.makeArgs(node2));
+        //find last message
+        t.truthy(res.response.new_messages, 'node2 should have at least one message')
+        //extract the last message sent to node2
+        const lastMessage2 = res.response.new_messages[res.response.new_messages.length-1]
+        //get media key from message
+        const node2MediaKey = lastMessage2.media_key
+    
+        const decryptMediaKey = rsa.decrypt(node2.privkey, node2MediaKey)
+    
+        //get media token
+        var token = await h.getToken(t, node2)
+    
+        const url = `https://memes.sphinx.chat/file/${lastMessage2.media_token}`
+    
+        const res2 = await fetch(url, {headers: {Authorization: `Bearer ${token}`}})
+        const blob = await res2.buffer()
+    
+        //media_key needs to be decrypted with your private key
+        const dec = RNCryptor.Decrypt(blob.toString("base64"), decryptMediaKey)
+        // const b64 = dec.toString('base64')
+        // //check equality b64 to b64
+        t.true(dec.toString("base64") === image)
+    
 
 
     return true
--- test-ava/test-functions/image-test.js
+++ test-ava/test-functions/image-test.js
@@ -15,12 +15,22 @@ async function imageTest(t, index1, index2) {
         //NODE1 SEND IMAGE TO NODE2
         const image = b.greenSquare
         const imageSent = await f.sendImage(t, node1, node2, image)
-        t.true(imageSent, "message should have been sent")
+        t.true(imageSent, "image should have been sent")
         
         //NODE2 SENDS AN IMAGE TO NODE1
         const image2 = b.pinkSquare
         const imageSent2 = await f.sendImage(t, node2, node1, image2)
-        t.true(imageSent2, "message should have been sent")
+        t.true(imageSent2, "image should have been sent")
+
+            //NODE1 SEND IMAGE TO NODE2
+    const price = 11
+    const paidImageSent = await f.sendImage(t, node1, node2, image, null, price)
+    t.true(paidImageSent, "paid image should have been sent")
+    
+    //NODE2 SENDS AN IMAGE TO NODE1
+    const price2 = 12
+    const paidImageSent2 = await f.sendImage(t, node2, node1, image2, null, price2)
+    t.true(paidImageSent2, "paid image should have been sent")
         
         //NODE1 AND NODE2 DELETE EACH OTHER AS CONTACTS
         let deletion = await f.deleteContacts(t, node1, node2)
