63bc26ed47da87ed4081ede69f7925162accbe97
CypherPoet <CypherPoet@gmail.com>
2021-04-06 14:03:01 -0500

    remove commented-out fetch logger

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ import TrackPlayer from 'react-native-track-player';
 
 LogBox.ignoreLogs(['Require cycle:'])
 
-// fetch logger
-// global._fetch = fetch;
-// global.fetch = function (uri, options, ...args) {
-//   return global._fetch(uri, options, ...args).then((response) => {
-//     // console.log('-> fetch:', { request: { uri, options, ...args }, response });
-//     return response;
-//   });
-// };
-
 AppRegistry.registerComponent('Sphinx', () => App)
 TrackPlayer.registerPlaybackService(() => require('./src/trackPlayer.js'));
 
