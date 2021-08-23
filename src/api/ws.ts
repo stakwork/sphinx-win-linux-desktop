c9fe0e8e75c7bc5a6857092535be304b8fe6f7a7
Evanfeenstra <evanfeenstra@gmail.com>
2021-08-23 15:28:32 -0700

    nbeter ws receiver

--- src/api/ws.ts
+++ src/api/ws.ts
@@ -1,55 +1,60 @@
-import socketio from 'socket.io-client';
+import socketio from "socket.io-client";
 
-type WSMessage = {[k: string]: any}
+type WSMessage = { [k: string]: any };
 
-type DataHandler = (data: any) => void
+type DataHandler = (data: any) => void;
 
-let handlers: {[k: string]: DataHandler} = {}
+let handlers: { [k: string]: DataHandler } = {};
 
-export function registerWsHandlers(hs: {[k: string]: DataHandler}) {
-  handlers = hs
+export function registerWsHandlers(hs: { [k: string]: DataHandler }) {
+  handlers = hs;
 }
 
-let io: any = null
+let io: any = null;
 
-export function connectWebSocket(ip: string, authToken:string, connectedCallback?:Function, disconnectCallback?:Function) {
-  if(io) {
-    return // dont reconnect if already exists
+export function connectWebSocket(
+  ip: string,
+  authToken: string,
+  connectedCallback?: Function,
+  disconnectCallback?: Function
+) {
+  if (io) {
+    return; // dont reconnect if already exists
   }
-  
+
   io = socketio.connect(ip, {
-    reconnection:true,
+    reconnection: true,
     transportOptions: {
       polling: {
         extraHeaders: {
-          'x-user-token': authToken
-        }
-      }
-    }
-  })
-
-  io.on('connect', socket => {
-    console.log("=> socketio connected!")
-    if(connectedCallback) connectedCallback()
-  })
-
-  io.on('disconnect', socket => {
-    if(disconnectCallback) disconnectCallback()
-  })
-
-  io.on('message', (data) => {
-    try {
-      let msg: WSMessage = JSON.parse(data)
-      let typ = msg.type
-      if(typ==='delete') typ='deleteMessage'
-      let handler = handlers[typ]
-      if (handler) {
-        handler(msg)
-      }
-    } catch(e) {}
+          "x-user-token": authToken,
+        },
+      },
+    },
   });
 
-  io.on('error',function(e){
-    console.log('socketio error',e)
-  })
+  io.on("connect", (socket) => {
+    console.log("=> socketio connected!");
+    if (connectedCallback) connectedCallback();
+  });
+
+  io.on("disconnect", (socket) => {
+    if (disconnectCallback) disconnectCallback();
+  });
+
+  io.on("message", (data) => {
+    try {
+      let msg: WSMessage = JSON.parse(data);
+      let typ = msg.type;
+      if (typ === "delete") typ = "deleteMessage";
+      let handler = handlers[typ];
+      if (handler) {
+        handler(msg);
+      }
+    } catch (e) {}
+  });
+
+  io.on("error", function (e) {
+    console.log("socketio error", e);
+  });
 }
--- src/store/websocketHandlers.ts
+++ src/store/websocketHandlers.ts
@@ -1,141 +1,142 @@
-import { msgStore } from './msg'
-import { contactStore } from './contacts'
-import { uiStore } from './ui'
-import { chatStore } from './chats'
+import { msgStore } from "./msg";
+import { contactStore } from "./contacts";
+import { uiStore } from "./ui";
+import { chatStore } from "./chats";
 
-export function contact(data) { // on contact_key msg
-  log('[ws] contact', data)
-  contactStore.contactUpdated(data.response)
+export function contact(data) {
+  // on contact_key msg
+  log("[ws] contact", data);
+  contactStore.contactUpdated(data.response);
 }
 
 export function invite(data) {
-  log('[ws] invite', data)
-  contactStore.updateInvite(data.response)
+  log("[ws] invite", data);
+  contactStore.updateInvite(data.response);
 }
 
 export function invoice_payment(data) {
-  log('[ws] invoice_payment', data.response)
+  log("[ws] invoice_payment", data.response);
   if (data.response && data.response.invoice) {
-    uiStore.setLastPaidInvoice(data.response.invoice)
+    uiStore.setLastPaidInvoice(data.response.invoice);
   }
 }
 
 /* sphinx types */
 
 export function message(data) {
-  log('[ws] message', data.response)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] message", data.response);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function confirmation(data) {
-  log("[ws] confirmation", data)
-  msgStore.setMessageAsReceived(data.response)
+  log("[ws] confirmation", data);
+  msgStore.setMessageAsReceived(data.response);
 }
 
 export function invoice(data) {
-  log("[ws] invoice", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] invoice", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function payment(data) {
-  log("[ws] payment", data)
-  msgStore.invoicePaid(data.response)
+  log("[ws] payment", data);
+  msgStore.invoicePaid(data.response);
 }
 
 export function cancellation(data) {
-  log("[ws] cancellation", data)
+  log("[ws] cancellation", data);
 }
 
 export function direct_payment(data) {
-  log("[ws] direct_payment", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] direct_payment", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function attachment(data) {
-  log("[ws] attachment", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] attachment", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function purchase(data) {
-  log("[ws] purchase", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] purchase", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function purchase_accept(data) {
-  log("[ws] purchase_accept", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] purchase_accept", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function purchase_deny(data) {
-  log("[ws] purchase_deny", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] purchase_deny", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function keysend(data) {
-  log("[ws] keysend", data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] keysend", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function group_create(data) {
-  log("[ws] group_create", data)
-  chatStore.gotChat(data.response.chat)
+  log("[ws] group_create", data);
+  chatStore.gotChat(data.response.chat);
 }
 
 export function group_join(data) {
-  log("[ws] group_join", data)
-  const msg = data.response && data.response.message
+  log("[ws] group_join", data);
+  const msg = data.response && data.response.message;
   if (msg && data.response.chat) {
-    msg.chat = data.response.chat
-    msgStore.gotNewMessageFromWS(msg)
+    msg.chat = data.response.chat;
+    msgStore.gotNewMessageFromWS(msg);
   }
 }
 
 export function group_leave(data) {
-  log("[ws] group_leave", data)
-  const msg = data.response && data.response.message
+  log("[ws] group_leave", data);
+  const msg = data.response && data.response.message;
   if (msg && data.response.chat) {
-    msg.chat = data.response.chat
-    msgStore.gotNewMessageFromWS(msg)
+    msg.chat = data.response.chat;
+    msgStore.gotNewMessageFromWS(msg);
   }
 }
 
 export function deleteMessage(data) {
-  log('[ws] delete message', data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] delete message", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function member_request(data) {
-  log('[ws] member_request message', data)
-  if (!data.response.message) return
-  msgStore.gotNewMessageFromWS(data.response.message)
+  log("[ws] member_request message", data);
+  if (!data.response.message) return;
+  msgStore.gotNewMessageFromWS(data.response.message);
 }
 
 export function member_approve(data) {
-  log('[ws] member_approve message', data)
-  if (!data.response.message) return
-  msgStore.gotNewMessageFromWS(data.response.message)
+  log("[ws] member_approve message", data);
+  if (!data.response.message) return;
+  msgStore.gotNewMessageFromWS(data.response.message);
 }
 
 export function member_reject(data) {
-  log('[ws] member_reject message', data)
-  if (!data.response.message) return
-  msgStore.gotNewMessageFromWS(data.response.message)
+  log("[ws] member_reject message", data);
+  if (!data.response.message) return;
+  msgStore.gotNewMessageFromWS(data.response.message);
 }
 
 export function bot_res(data) {
-  log('[ws] bot_res', data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] bot_res", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
 export function boost(data) {
-  log('[ws] boost', data)
-  msgStore.gotNewMessageFromWS(data.response)
+  log("[ws] boost", data);
+  msgStore.gotNewMessageFromWS(data.response);
 }
 
-const oktolog = false
+const oktolog = false;
 
 function log(a, b) {
   if (oktolog) {
-    console.log(a, b)
+    console.log(a, b);
   }
 }
