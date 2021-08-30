23bf362983cb02b5ed9b5a8be392fff80218c568
Evanfeenstra <evanfeenstra@gmail.com>
2021-08-30 16:42:18 -0700

    boostrow fixes

--- web/chat/msg/boostRow.tsx
+++ web/chat/msg/boostRow.tsx
@@ -1,130 +1,140 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import theme from '../../theme'
-import littleFireword from '../../../src/animations/little-firework.json'
-import Lottie from 'react-lottie';
-import moment from 'moment'
-import {BoostIcon} from '../pod/icons'
-import AvatarsRow from '../../utils/avatarsRow'
+import React, { useState } from "react";
+import styled from "styled-components";
+import theme from "../../theme";
+import littleFireword from "../../../src/animations/little-firework.json";
+import Lottie from "react-lottie";
+import moment from "moment";
+import { BoostIcon } from "../pod/icons";
+import AvatarsRow from "../../utils/avatarsRow";
 
 // https://lottiefiles.com/3074-fireworks
 
 const lottieOptions = {
   loop: false,
-  autoplay: true, 
+  autoplay: true,
   animationData: littleFireword,
   rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
-export default function BoostRow(props){
-  const {boosts,boosts_total_sats,isMe,myid} = props
+export default function BoostRow(props) {
+  const { boosts, boosts_total_sats, isMe, myid } = props;
 
-  const bg = isMe ? theme.highlight : theme.extraDeep
+  const bg = isMe ? theme.highlight : theme.extraDeep;
 
-  const [anim,setAnim] = useState(false)
+  const [anim, setAnim] = useState(false);
   function play() {
-    return
+    return;
     requestAnimationFrame(async () => {
-      setAnim(true)
-      await sleep(660)
-      setAnim(false)
-    })
+      setAnim(true);
+      await sleep(660);
+      setAnim(false);
+    });
   }
 
-  let fromText
-  const showAvatars = boosts && boosts.length>1
-  if(showAvatars) {
-    if(boosts.length>3) fromText = `+${boosts.length-3}`
+  let fromText;
+  const showAvatars = boosts && boosts.length > 1;
+  if (showAvatars) {
+    if (boosts.length > 3) fromText = `+${boosts.length - 3}`;
   } else {
-    fromText = boosts && boosts.length && (boosts[0].sender===myid?'Me':boosts[0].sender_alias)
+    fromText =
+      boosts &&
+      boosts.length &&
+      (boosts[0].sender === myid ? "Me" : boosts[0].sender_alias);
   }
-  return <Wrap onClick={play}>
-    <LottieWrap>
-      {anim && <Lottie options={lottieOptions}
-        height={100}
-        width={100}
-      />}
-    </LottieWrap>
-    <Box>
-      <BoxLeft>
-        <Boost />
-        <BoxText>{boosts_total_sats}</BoxText>
-        <BoxSatsText>sats</BoxSatsText>
-      </BoxLeft>
-      <BoxRight>
-        {showAvatars && <Avatars>
-          <AvatarsRow aliases={boosts.map(b=>b.sender_alias)} borderColor={bg} />
-        </Avatars>}
-        {(fromText?true:false) && <FromText>{fromText}</FromText>}
-      </BoxRight>
-    </Box>
-  </Wrap>
+  // console.log("BOOST ROW!!!!", boosts_total_sats, boosts);
+  return (
+    <Wrap onClick={play}>
+      <LottieWrap>
+        {anim && <Lottie options={lottieOptions} height={100} width={100} />}
+      </LottieWrap>
+      <Box>
+        <BoxLeft>
+          <Boost />
+          <BoxText>{boosts_total_sats}</BoxText>
+          <BoxSatsText>sats</BoxSatsText>
+        </BoxLeft>
+        <BoxRight>
+          {showAvatars && (
+            <Avatars>
+              <AvatarsRow
+                aliases={boosts.map((b) => b.sender_alias)}
+                borderColor={bg}
+              />
+            </Avatars>
+          )}
+          {(fromText ? true : false) && <FromText>{fromText}</FromText>}
+        </BoxRight>
+      </Box>
+    </Wrap>
+  );
 }
 
-function Boost(){
-  return <BoostGreen>
-    <BoostIcon style={{height:20,width:20}} />
-  </BoostGreen>
+function Boost() {
+  return (
+    <BoostGreen>
+      <BoostIcon style={{ height: 20, width: 20 }} />
+    </BoostGreen>
+  );
 }
 const BoostGreen = styled.div`
-  background:#48c998;
-  height:18px;
-  width:18px;
-  border-radius:3px;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-`
+  background: #48c998;
+  height: 18px;
+  width: 18px;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Wrap = styled.div`
-  display:flex;
-  max-height:24px;
-  align-items:center;
-  margin:2px 0;
-  position:relative;
-`
+  display: flex;
+  max-height: 24px;
+  align-items: center;
+  margin: 2px 0;
+  position: relative;
+`;
 const Box = styled.div`
-  border-radius:4px;
-  height:19px;
-  font-size:11px;
-  display:flex;
-  align-items:center;
-  margin-top:7px;
-  justify-content:space-between;
-  width:100%;
-`
+  border-radius: 4px;
+  height: 19px;
+  font-size: 11px;
+  display: flex;
+  align-items: center;
+  margin-top: 7px;
+  justify-content: space-between;
+  width: 100%;
+`;
 const BoxLeft = styled.div`
-  display:flex;
-  align-items:center;
-`
+  display: flex;
+  align-items: center;
+`;
 const BoxRight = styled.div`
-  display:flex;
-  align-items:center;
-  color:#bbb;
-`
+  display: flex;
+  align-items: center;
+  color: #bbb;
+`;
 const BoxText = styled.div`
-  margin-left:8px;
-`
+  margin-left: 8px;
+`;
 const BoxSatsText = styled.div`
-  margin-left:3px;
-  color:grey;
-`
+  margin-left: 3px;
+  color: grey;
+`;
 const LottieWrap = styled.div`
-  position:absolute;
-  top:-42px;
-  left:24px;
-`
+  position: absolute;
+  top: -42px;
+  left: 24px;
+`;
 const Avatars = styled.div`
-  display:flex;
-  align-items:center;
-`
+  display: flex;
+  align-items: center;
+`;
 const FromText = styled.div`
-  display:flex;
-  align-items:center;
-  margin-left:5px;
-`
+  display: flex;
+  align-items: center;
+  margin-left: 5px;
+`;
 async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- web/chat/msg/textMsg.tsx
+++ web/chat/msg/textMsg.tsx
@@ -1,131 +1,162 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import { ReactTinyLink } from 'react-tiny-link-electron'
-import { useParsedGiphyMsg, useParsedClipMsg } from '../../../src/store/hooks/msg'
-import Linkify from 'react-linkify';
-import * as ipc from '../../crypto/ipc'
-import { useHasLink } from './hooks'
-import Clip from './clipMsg'
-import JitsiMsg from './jitsiMsg'
-import Boost from './boostMsg'
-import TribeMsg from './tribeMsg'
-import BoostRow from './boostRow'
-import ContactMsg from './contactMsg'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { ReactTinyLink } from "react-tiny-link-electron";
+import {
+  useParsedGiphyMsg,
+  useParsedClipMsg,
+} from "../../../src/store/hooks/msg";
+import Linkify from "react-linkify";
+import * as ipc from "../../crypto/ipc";
+import { useHasLink } from "./hooks";
+import Clip from "./clipMsg";
+import JitsiMsg from "./jitsiMsg";
+import Boost from "./boostMsg";
+import TribeMsg from "./tribeMsg";
+import BoostRow from "./boostRow";
+import ContactMsg from "./contactMsg";
 
 export default function TextMsg(props) {
-  const { message_content, sender, joinTribe } = props
-  const isMe = sender === props.myid
-  const link = useHasLink(props)
-  const hasLink = message_content && link
+  const { message_content, sender, joinTribe } = props;
+  const isMe = sender === props.myid;
+  const link = useHasLink(props);
+  const hasLink = message_content && link;
   if (hasLink) {
-    return <Wrap {...props}>
-      <LinkifyWrapper>
-        <Linkify componentDecorator={(decoratedHref, decoratedText, key) => (
-          <a target="blank" href={decoratedHref} key={key} onClick={e => {
-            e.preventDefault();
-            ipc.send(`link`, {link})
-        }} >
-            {decoratedText}
-        </a>
-    )}>{message_content}</Linkify>
-      </LinkifyWrapper >
-      <ReactTinyLink
-        cardSize="small"
-        showGraphic={true}
-        maxLine={2}
-        minLine={1}
-        url={link}
-      />
-    </Wrap>
+    return (
+      <Wrap {...props}>
+        <LinkifyWrapper>
+          <Linkify
+            componentDecorator={(decoratedHref, decoratedText, key) => (
+              <a
+                target="blank"
+                href={decoratedHref}
+                key={key}
+                onClick={(e) => {
+                  e.preventDefault();
+                  ipc.send(`link`, { link });
+                }}
+              >
+                {decoratedText}
+              </a>
+            )}
+          >
+            {message_content}
+          </Linkify>
+        </LinkifyWrapper>
+        <ReactTinyLink
+          cardSize="small"
+          showGraphic={true}
+          maxLine={2}
+          minLine={1}
+          url={link}
+        />
+      </Wrap>
+    );
   }
 
-  const isTribe = message_content &&
-  message_content.startsWith('sphinx.chat://?action=tribe')
-  if(isTribe) {
-    return <TribeMsg {...props} />
+  const isTribe =
+    message_content &&
+    message_content.startsWith("sphinx.chat://?action=tribe");
+  if (isTribe) {
+    return <TribeMsg {...props} />;
   }
 
-  const contactRegex = /^[0-9a-fA-F]{66}$/
+  const contactRegex = /^[0-9a-fA-F]{66}$/;
   if (contactRegex.test(message_content)) {
-    return <ContactMsg {...props} />
+    return <ContactMsg {...props} />;
   }
 
-  const isGiphy = message_content && message_content.startsWith('giphy::')
+  const isGiphy = message_content && message_content.startsWith("giphy::");
   if (isGiphy) {
-    const { url, aspectRatio, text } = useParsedGiphyMsg(message_content)
-    return <div>
-      <GIF src={url} aspectRatio={aspectRatio} />
-      <TextWrap>{text}</TextWrap>
-    </div>
+    const { url, aspectRatio, text } = useParsedGiphyMsg(message_content);
+    return (
+      <div>
+        <GIF src={url} aspectRatio={aspectRatio} />
+        <TextWrap>{text}</TextWrap>
+      </div>
+    );
   }
 
-  const isClip = message_content && message_content.startsWith('clip::')
+  const isClip = message_content && message_content.startsWith("clip::");
   if (isClip) {
-    const params = useParsedClipMsg(message_content)
-    return <Clip {...params} isMe={isMe} uuid={props.uuid} />
+    const params = useParsedClipMsg(message_content);
+    return <Clip {...params} isMe={isMe} uuid={props.uuid} />;
   }
-  const isBoost = message_content && message_content.startsWith('boost::')
+  const isBoost = message_content && message_content.startsWith("boost::");
   if (isBoost) {
-    return <Boost {...props} isMe={isMe} />
+    return <Boost {...props} isMe={isMe} />;
   }
 
-  const isJitsi = message_content && message_content.startsWith('https://jitsi.sphinx.chat/')
-  if(isJitsi) {
-    return <JitsiMsg link={message_content} onCopy={props.onCopy} />
+  const isJitsi =
+    message_content && message_content.startsWith("https://jitsi.sphinx.chat/");
+  if (isJitsi) {
+    return <JitsiMsg link={message_content} onCopy={props.onCopy} />;
   }
 
-  const emo_regex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])$/;
+  const emo_regex =
+    /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])$/;
   if (emo_regex.test(message_content)) {
-    return <Wrap {...props} style={{ fontSize: 40 }}>{message_content}</Wrap>
+    return (
+      <Wrap {...props} style={{ fontSize: 40 }}>
+        {message_content}
+      </Wrap>
+    );
   }
-
-  return <Wrap {...props}>{message_content}</Wrap>
+  return <Wrap {...props}>{message_content}</Wrap>;
 }
 
-function Wrap(props:{children:any,style?:Object,boosts_total_sats:number,boosts:any,sender:number, myid:number}){
-  const isMe = props.sender===props.myid
-  const {children,style,boosts_total_sats,boosts} = props
-  return <WrapDiv style={style}>
-    {children}
-    {boosts_total_sats && <BoostRow {...props} isMe={isMe} />}
-  </WrapDiv>
+function Wrap(props: {
+  children: any;
+  style?: Object;
+  boosts_total_sats: number;
+  boosts: any;
+  sender: number;
+  myid: number;
+}) {
+  const isMe = props.sender === props.myid;
+  const { children, style, boosts_total_sats, boosts } = props;
+  return (
+    <WrapDiv style={style}>
+      {children}
+      {(boosts_total_sats ? true : false) && (
+        <BoostRow {...props} isMe={isMe} />
+      )}
+    </WrapDiv>
+  );
 }
 
 const WrapDiv = styled.div`
-  padding:16px;
-  max-width:440px;
+  padding: 16px;
+  max-width: 440px;
   word-break: break-word;
-`
+`;
 const LinkifyWrapper = styled.div`
   margin-bottom: 10px;
   & a {
     color: #618aff;
   }
-`
+`;
 const Link = styled.a`
-  display:block;
-  padding:16px;
-  max-width:440px;
+  display: block;
+  padding: 16px;
+  max-width: 440px;
   word-break: break-word;
-  color:#6089ff;
-  &:focus{
-    color:#6089ff;
+  color: #6089ff;
+  &:focus {
+    color: #6089ff;
   }
-  &:visited{
-    color:#6089ff;
+  &:visited {
+    color: #6089ff;
   }
-`
-const LinkWrap = styled.div`
-  
-`
+`;
+const LinkWrap = styled.div``;
 const GIF = styled.div`
-  background-image: url(${p => p.src});
+  background-image: url(${(p) => p.src});
   background-position: center;
   background-repeat: no-repeat;
-  background-size:cover;
-  height:220px;
-  width:${p => 220 * (p.aspectRatio || 1)}px;
-`
+  background-size: cover;
+  height: 220px;
+  width: ${(p) => 220 * (p.aspectRatio || 1)}px;
+`;
 const TextWrap = styled.div`
   padding: 15px;
-`
+`;
