2ae034db0ee2800380798b56e35fe7ce70a79e96
EvanFeenstra1 <evanfeenstra@gmail.com>
2021-09-17 12:31:09 -0700

    test config

--- test-ava/test-config.js
+++ test-ava/test-config.js
@@ -2,14 +2,14 @@
 
 //enter index of nodes to test from nodes.json, and whether to iterate or not
 const run = {
-    oneNode: [5],
-    twoNodes: [5, 6],
-    threeNodes: [5, 6, 7],
-    active: "threeNodes",
-    iterate: false,
-    memeHost: "localhost:5000",
-    tribeHost: "localhost:5002",
-    allowedFee: 4,
-}
+  oneNode: [0],
+  twoNodes: [0, 1],
+  threeNodes: [0, 1, 2],
+  active: "threeNodes",
+  iterate: false,
+  memeHost: "localhost:5000",
+  tribeHost: "localhost:5002",
+  allowedFee: 4,
+};
 
-module.exports = run
+module.exports = run;
